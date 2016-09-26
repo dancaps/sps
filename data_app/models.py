b548b2f4b7d260ea393e15e0a2a04e8d99517ca4
dancaps <danny.caperton@gmail.com>
2016-09-25 18:46:30 -0600

    added a function to populate the db

--- data_app/models.py
+++ data_app/models.py
@@ -10,10 +10,10 @@ class Customers(models.Model):
     state = models.CharField(max_length=2)
     zip = models.PositiveSmallIntegerField()
     primary_phone = models.PositiveIntegerField()
-    seconard_phone = models.PositiveIntegerField(blank=True)
+    #secondary_phone = models.PositiveIntegerField(blank=True)
 
-    def __str__(self):
-        return 'First Name : ' + self.human_first_name #'/n' + 'Last Name : ' + self.human_last_name '/n' + 'Pet Name : ' + self.pet_name
+    #def __str__(self):
+    #    return 'First Name : ' + self.human_first_name #'/n' + 'Last Name : ' + self.human_last_name '/n' + 'Pet Name : ' + self.pet_name
 
     # email
     # vet_name
@@ -24,7 +24,15 @@ class Customers(models.Model):
     # left_rating
     # allows_pics
 
-#a = Customers(human_first_name='Brad', human_last_name='Long', pet_name='Benji', street_address='123 1st Rd.', city='Superior', state='CO', zip=12345, primary_phone =1234567890, secondary_phone=1987654321)
-
+def populate_db():
+    a = Customers(human_first_name='Brad', human_last_name='Long', pet_name='Benji', street_address='1 North Rd.',
+                  city='Superior', state='CO', zip=12345, primary_phone =9234567890,)
+    a.save()
+    b = Customers(human_first_name='Fred', human_last_name='Smith', pet_name='Fido', street_address='123 1st Rd.',
+                  city='Superior', state='CO', zip=12345, primary_phone =3234567890,)
+    b.save()
+    c = Customers(human_first_name='Kathy', human_last_name='Clark', pet_name='Bubbles', street_address='987 15th St.',
+                  city='Superior', state='CO', zip=12345, primary_phone =1234567890,)
+    c.save()
 
 
