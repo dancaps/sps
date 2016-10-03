303bd19181385b95ed1fe95a6155f5110a58bacd
Danny Caperton <danny.caperton@gmail.com>
2016-10-03 13:12:58 -0700

    playing with my models and fields

--- data_app/models.py
+++ data_app/models.py
@@ -2,6 +2,11 @@ from __future__ import unicode_literals
 from django.db import models
 from localflavor.us.models import PhoneNumberField, USStateField, USZipCodeField
 
+# Questions:
+# can I populate fields with the data from other fields? for example order mileage based on the num_visits * customer table mileage?
+# How do I make it so that my form only displays the pets that belong to the selected customer?
+# I'd like to see how through works
+
 class Customer(models.Model):
     signup_date = models.DateTimeField(auto_now_add=True)
     first_name = models.CharField(max_length=200)
@@ -10,13 +15,13 @@ class Customer(models.Model):
     city = models.CharField(max_length=200)
     state = USStateField()
     zip_code = USZipCodeField()
-    primary_phone = PhoneNumberField()
+    primary_phone = PhoneNumberField(blank=True, null=True)
     secondary_phone = PhoneNumberField(blank=True, null=True)
-    email = models.EmailField()
-    vet_name = models.CharField(max_length=200)
-    vet_phone = PhoneNumberField()
-    emergency_contact_name = models.CharField(max_length=200)
-    emergency_contact_phone = PhoneNumberField()
+    email = models.EmailField(blank=True, null=True)
+    vet_name = models.CharField(max_length=200, blank=True, null=True)
+    vet_phone = PhoneNumberField(blank=True, null=True)
+    emergency_contact_name = models.CharField(max_length=200, blank=True, null=True)
+    emergency_contact_phone = PhoneNumberField(blank=True, null=True)
     contract_on_file = models.BooleanField()
     left_rating = models.BooleanField()
     allows_pics = models.BooleanField()
@@ -32,80 +37,34 @@ class Pet(models.Model):
     animal_type = models.CharField(max_length=200)
 
     def __str__(self):
-        return 'Pet Name: {} - {} - Owner: {} {}'.format(self.name, self.animal_type, self.customer.first_name,
-                                                         self.customer.last_name)
+        return 'Owner: {} {} - Pet Name: {} - Animal: {}'.format(self.customer.first_name, self.customer.last_name,
+                                                                 self.name, self.animal_type)
+
 class Service(models.Model):
-    name = models.CharField(max_length=200, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return 'Service: {} - Price: ${}'.format(self.name, self.price)
 
 class Order(models.Model):
     order_date = models.DateTimeField(auto_now_add=True)
-    customer = models.ForeignKey(Customer, null=True)
+    customer = models.ForeignKey(Customer)
     start_date = models.DateField(auto_now=False)
     end_date = models.DateField(auto_now=False)
     total_visits = models.IntegerField(null=True)
-    total_mileage = models.IntegerField(null=True) #can I populate this with customer mileage * num_visits
-    amount_due = models.DecimalField(max_digits=10, decimal_places=2, null=True) #can I populate this services.price
-    pets = models.ManyToManyField(Pet)
+    total_mileage = models.IntegerField(null=True, blank=True) #can I populate this with customer mileage * num_visits
+    amount_due = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True) #can I populate this services.price
+    pets = models.ManyToManyField(Pet) #I only want the customers pets to show up.
     services = models.ForeignKey(Service, null=True)
+    paid = models.BooleanField(default=False)
 
     def __str__(self):
         #services = '-'.join([str(service.name) for service in self.services.get_queryset()])
-        return 'OrderID = {}, ' \
+        return 'Order Number = {}, ' \
                'Customer: {} {}, ' \
                'Start Date: {}, ' \
-               'End Date: {}'.format(self.id, self.customer.first_name, self.customer.last_name, self.start_date, self.end_date)
-
-def populate_db(cust=3):
-        count = 0
-        while count < cust:
-            a = Customer(first_name='Brad', last_name='Long', street_address='1 North Rd.',
-                          city='Superior', state='CO', zip_code=12345, primary_phone=9234567890, secondary_phone=5126941175,
-                          email='danny.caperton@gmail.com', vet_name='Billy Vetman', vet_phone=6549871321,
-                          emergency_contact_name='Helen Keller', emergency_contact_phone=6549873215, contract_on_file=True,
-                          left_rating=False, allows_pics=True, mileage=10)
-            a.save()
-            b = Customer(first_name='Mike', last_name='Long', street_address='1 North Rd.',
-                          city='Superior', state='CO', zip_code=12345, primary_phone=9234567890, secondary_phone=5126941175,
-                          email='mike@gmail.com', vet_name='Billy Vetman', vet_phone=6549871321,
-                          emergency_contact_name='Helen Keller', emergency_contact_phone=6549873215, contract_on_file=True,
-                          left_rating=False, allows_pics=True, mileage=7)
-            b.save()
-            c = Customer(first_name='Sarah', last_name='Long', street_address='1 North Rd.',
-                          city='Superior', state='CO', zip_code=12345, primary_phone=9234567890, secondary_phone=5126941175,
-                          email='sarah@gmail.com', vet_name='Billy Vetman', vet_phone=6549871321,
-                          emergency_contact_name='Helen Keller', emergency_contact_phone=6549873215, contract_on_file=True,
-                          left_rating=False, allows_pics=True, mileage=4, notes='shitty customer')
-            c.save()
-            d = Pet(name='Fido', animal_type='Dog', customer=a)
-            d.save()
-            e = Pet(name='Benji', animal_type='Dog', customer=a)
-            e.save()
-            f = Pet(name='Cat', animal_type='Cat', customer=a)
-            f.save()
-            g = Pet(name='Fido', animal_type='Dog', customer=b)
-            g.save()
-            h = Pet(name='Bubbles', animal_type='Fish', customer=b)
-            h.save()
-            i = Pet(name='Piggy', animal_type='Guinea Pig', customer=b)
-            i.save()
-            j = Pet(name='Fishy', animal_type='Fish', customer=c)
-            j.save()
-            count += 1
-
-        k = Service(name='Dog Walking', price=20.00)
-        k.save()
-        l = Service(name='Cat Sitting', price=15.00)
-        l.save()
-        m = Service(name='Overnight', price=65.00)
-        m.save()
-        n = Order(start_date='2017-01-01', customer=a, end_date='2017-02-01', total_visits=5, total_mileage=25,
-                  services=k, amount_due=100.00)
-        n.save()
-        # n.services.add(k)
-        # n.save()
-        n.pets.add(d)
-        n.save()
+               'End Date: {}, ' \
+               'Amount Due: {}, ' \
+               'Paid: {}'.format(self.id, self.customer.first_name, self.customer.last_name, self.start_date,
+                                 self.end_date, self.amount_due, self.paid)
---
+++ data_app/tools.py
@@ -0,0 +1,52 @@
+from data_app.models import *
+
+def populate_db(cust=3):
+    count = 0
+    while count < cust:
+        a = Customer(first_name='Brad', last_name='Long', street_address='1 North Rd.',
+                     city='Superior', state='CO', zip_code=12345, primary_phone=9234567890, secondary_phone=5126941175,
+                     email='danny.caperton@gmail.com', vet_name='Billy Vetman', vet_phone=6549871321,
+                     emergency_contact_name='Helen Keller', emergency_contact_phone=6549873215, contract_on_file=True,
+                     left_rating=False, allows_pics=True, mileage=10)
+        a.save()
+        b = Customer(first_name='Mike', last_name='Long', street_address='1 North Rd.',
+                     city='Superior', state='CO', zip_code=12345, primary_phone=9234567890, secondary_phone=5126941175,
+                     email='mike@gmail.com', vet_name='Billy Vetman', vet_phone=6549871321,
+                     emergency_contact_name='Helen Keller', emergency_contact_phone=6549873215, contract_on_file=True,
+                     left_rating=False, allows_pics=True, mileage=7)
+        b.save()
+        c = Customer(first_name='Sarah', last_name='Long', street_address='1 North Rd.',
+                     city='Superior', state='CO', zip_code=12345, primary_phone=9234567890, secondary_phone=5126941175,
+                     email='sarah@gmail.com', vet_name='Billy Vetman', vet_phone=6549871321,
+                     emergency_contact_name='Helen Keller', emergency_contact_phone=6549873215, contract_on_file=True,
+                     left_rating=False, allows_pics=True, mileage=4, notes='shitty customer')
+        c.save()
+        d = Pet(name='Fido', animal_type='Dog', customer=a)
+        d.save()
+        e = Pet(name='Benji', animal_type='Dog', customer=a)
+        e.save()
+        f = Pet(name='Cat', animal_type='Cat', customer=a)
+        f.save()
+        g = Pet(name='Fido', animal_type='Dog', customer=b)
+        g.save()
+        h = Pet(name='Bubbles', animal_type='Fish', customer=b)
+        h.save()
+        i = Pet(name='Piggy', animal_type='Guinea Pig', customer=b)
+        i.save()
+        j = Pet(name='Fishy', animal_type='Fish', customer=c)
+        j.save()
+        count += 1
+
+    k = Service(name='Dog Walking', price=20.00)
+    k.save()
+    l = Service(name='Cat Sitting', price=15.00)
+    l.save()
+    m = Service(name='Overnight', price=65.00)
+    m.save()
+    n = Order(start_date='2017-01-01', customer=a, end_date='2017-02-01', total_visits=5, total_mileage=25,
+              services=k, amount_due=100.00)
+    n.save()
+    # n.services.add(k)
+    # n.save()
+    n.pets.add(d)
+    n.save()
