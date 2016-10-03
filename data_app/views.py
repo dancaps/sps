28bdcc75fe57e75b4b42a7d08ed22503020624da
Danny Caperton <danny.caperton@gmail.com>
2016-10-03 15:01:55 -0700

    adding forms, stactic files and html

--- data_app/views.py
+++ data_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import auth
+
 
 def landing_page(request):
     html = "<h1>LANDING PAGE</h1>"
