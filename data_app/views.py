3220ac333d80afcdbeb6a091a899d2406bb6e383
dancaps <danny.caperton@gmail.com>
2016-10-04 10:38:39 -0600

    views and templates

--- data_app/views.py
+++ data_app/views.py
@@ -4,5 +4,5 @@ from django.contrib import auth
 
 
 def landing_page(request):
-    html = "<h1>LANDING PAGE</h1>"
-    return HttpResponse(html)
+    context = {'html': 'THIS IS MY NEW CODE!'}
+    return render(request, 'data_app.html', context)
--- sps/settings.py
+++ sps/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'sps.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'data_app', 'templates',)
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
